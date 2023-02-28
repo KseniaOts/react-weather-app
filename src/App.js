0f3df139e24494a7394ae2f38e0375de4bf3418e
KseniaOts <113545330+KseniaOts@users.noreply.github.com>
2023-02-28 20:02:55 +0200

    added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://spectacular-boba-8a9fc7.netlify.app/">Ksenia Ots</a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/KseniaOts/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
