cb59a070a39c513f5aca547e941d4131f0b8684e
KseniaOts <113545330+KseniaOts@users.noreply.github.com>
2023-03-11 20:04:49 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://spectacular-boba-8a9fc7.netlify.app/">Ksenia Ots</a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/KseniaOts/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div classname="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://spectacular-boba-8a9fc7.netlify.app/">Ksenia Ots</a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KseniaOts/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
