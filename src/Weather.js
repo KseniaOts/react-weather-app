be7d2e22bf1b591e95c0e9f2a02b4cf8e86a3cc9
KseniaOts <113545330+KseniaOts@users.noreply.github.com>
2023-04-16 13:50:24 +0300

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="row">
-        <div className="col-6">
-          <h1> New York</h1>
-          <span className="temperature">13</span>
-          <span className="unit">°C/°F</span>
-          <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAb5JREFUeNrtmtuNgzAQRSmBElwCJbgESuBzy6AESnAJlEAJ/PJHOqAD73g10QJa8sJ4huUiXSlyHMX3jM34lXnvsysrAwAAAAAAAAAAAAAAAAAAAIDfwg8ff/sypIrkSD3Jb6jnOqGuyRI/0QGQCUtqHxh+pvBbezoAbLzbYXytLgWI3QCokTmpiWh8rUYtADbfH2h+/p7IVQGgBhWkKYH5u8bwnyoAcORTmp9DyEUBJOz2m8NBGkAjaD76i/EtAJzqvBJZCQCdIgBdUgDKoh+tF7wDoFUIoE0CgBc2XqnyFAAqYZNhtVjyMLT8OZTVoX3DMBiSI00kz5q4zMQA4KRy/rPZHxksVsbXCt8VewH0QuZzanxQ/Ud0a478I/Pz+mYPAInoFxzh/gWDr8idCYBj83Uk8z+94CMAQhmgZABTRAD+LD0gzDYNj31/gBbZQRuAavZ2PwrAIjtoAlCx8ZI0Hmx+kR00pMFuZt4nltMwEbpHfxQAMGmYCtsEY35T4ouhTPBRsRw+C4Brb4hcfksMm6LYFsfBCI7GcDiK43FckMAVGVySuto1OdwUBQAAAAAAAAAAAAAAAAAA+Pf6Bm5v6/1bJGiwAAAAAElFTkSuQmCC"
-            alt="Mostly Cloudy"
-          />
-          <ul>
-            <li> Wednesday 07:00</li>
-            <li>Mostly clowdy</li>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 75%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <form>
-            <div className="row">
-              <div className="col-6">
-                <input
-                  type="search"
-                  placeholder="Enter a city"
-                  className="form-control"
-                />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="row">
+          <div className="col-6">
+            <h1> New York</h1>
+            <span className="temperature">13</span>
+            <span className="unit">°C/°F</span>
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAb5JREFUeNrtmtuNgzAQRSmBElwCJbgESuBzy6AESnAJlEAJ/PJHOqAD73g10QJa8sJ4huUiXSlyHMX3jM34lXnvsysrAwAAAAAAAAAAAAAAAAAAAIDfwg8ff/sypIrkSD3Jb6jnOqGuyRI/0QGQCUtqHxh+pvBbezoAbLzbYXytLgWI3QCokTmpiWh8rUYtADbfH2h+/p7IVQGgBhWkKYH5u8bwnyoAcORTmp9DyEUBJOz2m8NBGkAjaD76i/EtAJzqvBJZCQCdIgBdUgDKoh+tF7wDoFUIoE0CgBc2XqnyFAAqYZNhtVjyMLT8OZTVoX3DMBiSI00kz5q4zMQA4KRy/rPZHxksVsbXCt8VewH0QuZzanxQ/Ud0a478I/Pz+mYPAInoFxzh/gWDr8idCYBj83Uk8z+94CMAQhmgZABTRAD+LD0gzDYNj31/gBbZQRuAavZ2PwrAIjtoAlCx8ZI0Hmx+kR00pMFuZt4nltMwEbpHfxQAMGmYCtsEY35T4ouhTPBRsRw+C4Brb4hcfksMm6LYFsfBCI7GcDiK43FckMAVGVySuto1OdwUBQAAAAAAAAAAAAAAAAAA+Pf6Bm5v6/1bJGiwAAAAAElFTkSuQmCC"
+              alt="Mostly Cloudy"
+            />
+            <ul>
+              <li> Wednesday 07:00</li>
+              <li>Mostly clowdy</li>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 75%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <form>
+              <div className="row">
+                <div className="col-6">
+                  <input
+                    type="search"
+                    placeholder="Enter a city"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary"
+                  />
+                </div>
               </div>
-              <div className="col-3">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="btn btn-primary"
-                />
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1be85983e26932dcdad5b28a3c894628";
+    let city = "Tallinn";
+    let apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
