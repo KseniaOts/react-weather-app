09f7ec6b59c6c63bc1d5c3c1ec667d318757ef05
KseniaOts <113545330+KseniaOts@users.noreply.github.com>
2023-04-26 19:25:00 +0300

    Date and time added

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormatDate from "./FormatDate";
 import axios from "axios";
 import "./Weather.css";
 export default function Weather(props) {
@@ -9,7 +10,7 @@ export default function Weather(props) {
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Monday 20:05",
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
       wind: response.data.wind.speed,
@@ -29,7 +30,9 @@ export default function Weather(props) {
             <span className="unit">°C/°F</span>
             <img src={weatherData.iconUrl} alt={weatherData.description} />
             <ul>
-              <li> {weatherData.date}</li>
+              <li>
+                <FormatDate date={weatherData.date} />
+              </li>
               <li className="text-capitalize">{weatherData.description}</li>
 
               <li>Humidity: {weatherData.humidity}%</li>
